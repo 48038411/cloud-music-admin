1f71ef875c1b7e3206519c913307e1b663d29175
ytker <48038411@qq.com>
2020-04-20 21:29:54 +0800

    第一次提交云音乐后台

--- src/main.js
+++ src/main.js
@@ -1,12 +1,42 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
 
-Vue.config.productionTip = false;
+Vue.use(MuseUI)
+Vue.config.productionTip = false
+
+// 导航钩子，全局钩子
+router.beforeEach((to, from, next) => {
+  let token = localStorage.getItem('token')
+  let isLogin
+  // 若从本地取出的token为空，则未登录。否则登录
+  if (!token) {
+    isLogin = false
+  } else {
+    isLogin = true
+  }
+  // 如果未登录，则跳转到登录页面
+  if (!isLogin) {
+    if (to.path !== '/login') {
+      return next({ path: '/login' })
+    } else {
+      next()
+    }
+  } else {
+    if (to.path === '/login') {
+      return next({ path: '/' })
+    }
+    next()
+  }
+})
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  vuetify,
+  render: (h) => h(App)
+}).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,61 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
 
-Vue.use(Vuex);
-
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
-});
+const store = new Vuex.Store({
+  state: {
+    token: localStorage.getItem('token'),
+    // parse一下，因为user是我们json序列化过的数据
+    user: JSON.parse(localStorage.getItem('user')),
+    menuList: [
+      {
+        title: 'Dashboard',
+        icon: 'mdi-view-dashboard',
+        url: '/dashboard',
+        subMenus: []
+      },
+      {
+        title: '音乐管理',
+        icon: 'mdi-music',
+        // 因为是一个展开菜单，所以URL为空
+        url: '',
+        subMenus: [
+          {
+            title: '歌单管理',
+            icon: 'mdi-domain',
+            url: '/music-list',
+            // 没有权限
+            permissions: []
+          },
+          {
+            title: '歌曲管理',
+            icon: 'mdi-text',
+            url: '/music',
+            permissions: ['music:add', 'music:edit', 'music:delete']
+          }
+        ]
+      },
+      { title: 'about', icon: 'mdi-help-box', url: '/about', subMenus: [] }
+    ]
+  },
+  // 几个set方法，方便vuex更新
+  mutations: {
+    setToken(state, data) {
+      state.token = data
+    },
+    setUser(state, user) {
+      state.user = user
+    },
+    setMenuList(state, menuList) {
+      state.menuList = menuList
+    }
+  },
+  actions: {}
+})
+// export default new Vuex.Store({
+//   state: {},
+//   mutations: {},
+//   actions: {},
+//   modules: {}
+// });
+export default store
